82c07a159fc5fab02f0dbd531d0773db5c186d5c
Paulo <p.henriquerm@hotmail.com>
2019-02-04 17:54:48 -0300

    Initial commit

---
+++ app/src/main/java/com/exemplo/android/audioplayer/MainActivity.java
@@ -0,0 +1,21 @@
+package com.exemplo.android.audioplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mp = MediaPlayer.create(this, R.raw.all_about_you);
+
+
+    }
+}
