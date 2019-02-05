089f95be86181d96453d05a077c2cb5e7b5d1ab4
Paulo <p.henriquerm@hotmail.com>
2019-02-05 15:04:17 -0300

    Criando efeito de mudança de pause pra play e vice versa

--- app/src/main/java/com/exemplo/android/audioplayer/MainActivity.java
+++ app/src/main/java/com/exemplo/android/audioplayer/MainActivity.java
@@ -5,9 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer mp;
+    private MediaPlayer mp;
+    private ImageButton btn_play_pause;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +20,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mp = MediaPlayer.create(this, R.raw.all_about_you);
+        btn_play_pause = findViewById(R.id.btn_play_pause);
+
+        btn_play_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mp.isPlaying()){
+                    mp.pause();
+                    btn_play_pause.setImageResource(R.drawable.play);
+                }else{
+                    mp.start();
+                    btn_play_pause.setImageResource(R.drawable.pause);
+                }
+
+            }
+        });
 
 
     }
+
+
+
 }
